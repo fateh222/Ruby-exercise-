f716737c1d3a712a3d634b4f76f4f1fb8211bda8
risheak <dev07@173-168-100-51.res.bhn.net>
2017-11-07 20:49:03 +0530

    new files

---
+++ exercise_44.rb
@@ -0,0 +1,108 @@
+#Exercise 44: Inheritance Versus Composition
+
+class Parent
+
+  def implicit()
+    puts "PARENT implicit()"
+  end
+end
+
+class Child < Parent
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.implicit()
+son.implicit()
+
+
+
+puts "*" * 10
+
+
+class Parent
+
+  def override()
+    puts "PARENT override()"
+  end
+end
+
+class Child < Parent
+  def override()
+    puts "CHILD override()"
+  end
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.override()
+son.override()
+
+puts "*" * 10
+
+class Parent
+  def altered()
+    puts "PARENT altered()"
+  end
+end
+
+class Child < Parent
+  def altered()
+    puts "CHILD, BEFORE PARENT altered()"
+    super()
+    puts "CHILD, AFTER PARENT altered()"
+  end
+
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.altered()
+son.altered()
+
+
+puts "*" * 10
+
+
+class Parent
+
+  def override()
+    puts "PARENT override()"
+  end
+
+  def implicit()
+    puts "PARENT implicit()"
+  end
+
+  def altered()
+    puts "PARENT altered()"
+  end
+end
+
+class Child < Parent
+
+  def override()
+    puts "CHILD override()"
+  end
+
+  def altered()
+    puts "CHILD, BEFORE PARENT altered()"
+    super()
+    puts "CHILD, AFTER PARENT altered()"
+  end
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
